f9b8846a4fd5cbe9567828159d557db9fd244fb8
Moutlou <moutlou@outlook.com>
2019-04-19 10:29:04 +0300

    login form ready

--- src/App.js
+++ src/App.js
@@ -1,27 +1,59 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class Login extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+    return (      
+      <div className="container">
+        <div className="row" style={{marginTop: 50}}>
+          <div className = "col-md-4"></div>
+          <div className = "col-md-4">
+            <form>
+              <div className="form-group">
+                <label for="userName">Username</label>
+                <input type="text" class="form-control" id="userName" placeholder="Enter username"></input>              
+              </div>
+              <div className="form-group">
+                <label for="password">Password</label>
+                <input type="password" class="form-control" id="password" placeholder="Enter password"></input>              
+              </div>
+              <button type="submit" class="btn btn-primary">Login</button>
+            </form>
+          </div>
+          <div className = "col-md-4"></div>
+        </div>
       </div>
     );
+  }
+}
+class App extends Component {
+  constructor(props)
+  {
+    super(props);
+    this.state = {
+      userId : null,
+      accessToken : null,
+    }
+
+    this.isLoggedIn = ()=> {
+      return this.state.userId != null && this.state.accessToken != null;
+    }
+  }
+  render() {
+    if(this.isLoggedIn() === false) {
+      return (
+        <div className="App">
+            <Login/>
+        </div>
+      )
+    }
+    else {
+      return (
+        <div className="App">
+            OK
+        </div>
+      )
+    }
   }
 }
 
